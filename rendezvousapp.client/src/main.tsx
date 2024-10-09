f453ac9e738dc4eae7e8bdca9d4d9903dc46755e
Wongsathorn Chengcharoen <wongsathorn.cheng@gmail.com>
2024-10-09 15:29:12 +0700

    Use ThemProvider

--- rendezvousapp.client/src/main.tsx
+++ rendezvousapp.client/src/main.tsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createTheme, ThemeProvider } from '@mui/material'
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#395536"
+    },
+    secondary: {
+      main: "#988265"
+    },
+  },
+  typography: {
+    fontFamily: 'Montserrat',
+    h1: {
+      fontSize: '2rem',
+      fontWeight: 600,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
