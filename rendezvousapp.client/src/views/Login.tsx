c14ec7cc91c0d23aa5149d275b2ea7d8b1b505f7
Wongsathorn Chengcharoen <wongsathorn.cheng@gmail.com>
2024-10-09 20:40:32 +0700

    Bring back format

--- rendezvousapp.client/src/views/Login.tsx
+++ rendezvousapp.client/src/views/Login.tsx
@@ -1,9 +1,33 @@
-import { Container, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
+import logo from '../assets/logo.png';
 
 function Login() {
     return (
-        <Container>
-            <Typography variant="h1">Login Page</Typography>
+        <Container
+            sx = {{ 
+                overflow: 'hidden', 
+                height: '100vh',
+            }}
+        >
+            <Container
+                sx = {{ 
+                    display: 'flex', 
+                    flex: 'row', 
+                    alignItems: 'center', 
+                    justifyContent: 'center',
+                    height: '100%',
+                }}
+            >
+                <Box
+                    component="img"
+                    alt = "Rendezvous Logo"
+                    src = {logo}
+                    sx={{ width: 500, height: 500, mr: 2 }}
+                />
+
+                <Typography>Login Form</Typography>
+
+            </Container>
         </Container>
     );
 }
