b30efddac369ed54a5e4fd03b4a3db76352f2df0
Rqkko <rakko26@gmail.com>
2024-10-13 15:38:24 +0700

    Update UserController.cs (GetUser)
    Functional

--- RendezvousApp.Server/Controllers/UserController.cs
+++ RendezvousApp.Server/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using MySql.Data.MySqlClient;
 using Org.BouncyCastle.Security;
+using RendezvousApp.Server.Entities;
 
 namespace RendezvousApp.Server.Controllers
 {
@@ -22,14 +23,61 @@ namespace RendezvousApp.Server.Controllers
         [HttpGet("GetUser/{contact}/{password}")] // contact is email/phoneNumber
         public ActionResult GetUser(string contact, string password)
         {
-            return Ok(contact + "_" + password);
+            User? user = null;
+
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query;
+                if (contact.Contains("@"))
+                {
+                    query = "SELECT * FROM Users WHERE email = @contact";
+                }
+                else
+                {
+                    query = "SELECT * FROM Users WHERE phone = @contact";
+                }
+                
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@contact", contact);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        user = new User
+                        {
+                            UserId = (int) reader["userId"],
+                            Firstname = (string) reader["firstname"],
+                            Lastname = (string) reader["lastname"],
+                            Phone = (string) reader["phone"],
+                            Email = (string) reader["email"],
+                            Password = (string) reader["password"],
+                        };
+                    }
+                }
+
+                if (user == null)
+                {
+                    return NotFound("User not found");
+                }
+            }
+
+            // Check if password is correct
+            if (user.Password != password)
+            {
+                return Unauthorized("Incorrect password");
+            }
+
+            return Ok(user);
         }
 
         [HttpGet("GetFirstname")]
         public ActionResult GetFirstname()
         {
             string? firstname = null;
-
+ 
             // MySqlConnection connection = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection"));
             MySqlConnection connection = new MySqlConnection(_connectionString);
             connection.Open();
