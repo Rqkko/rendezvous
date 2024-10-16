6f11efdccd864d5a364df071e3390acc1040a1e3
Rqkko <rakko26@gmail.com>
2024-10-16 14:34:13 +0700

    Create "GetAllLocations" in EventController.cs

--- RendezvousApp.Server/Controllers/EventController.cs
+++ RendezvousApp.Server/Controllers/EventController.cs
@@ -20,6 +20,47 @@ public class EventController : ControllerBase
     [HttpGet("GetAllLocations")]
     public ActionResult GetAllLocations()
     {
-        return Ok("Working");
+        List<Location> locations = new();
+
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            string query = @"
+                SELECT L.*, A.* FROM Locations AS L
+                JOIN Addresses AS A ON L.locationId = A.locationId;
+            ";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    locations.Add(new Location
+                        {
+                            LocationId = (int) reader["locationId"],
+                            LocationName = (string) reader["locationName"],
+                            LocationDescription = (string) reader["locationDescription"],
+                            Area = (int) reader["area"],
+                            Capacity = (int) reader["capacity"],
+                            Cost = (int) reader["cost"],
+                            LocationImage = (byte[]) reader["locationImage"],
+                            Province = (string) reader["province"],
+                            PostalCode = (string) reader["postalCode"],
+                            Additional = (string) reader["additional"],
+                            AdminId = (int) reader["adminId"],
+                        }
+                    );
+                }
+            }
+
+            if (locations.Count == 0)
+            {
+                return NotFound(new { message = "No Location Found" });
+            }
+
+            Console.WriteLine(locations.Count + " Locations Found");
+            return Ok(locations);
+        }
     }
 }
