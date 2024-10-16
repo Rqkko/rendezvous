1888c93a53b5fc4d946379ae2604b9cfb991c5c8
Rqkko <rakko26@gmail.com>
2024-10-16 13:38:37 +0700

    Create Location.cs

---
+++ RendezvousApp.Server/Entities/Location.cs
@@ -0,0 +1,14 @@
+namespace RendezvousApp.Server.Entities
+{
+    public class Location
+    {
+        public int LocationId { get; set; }
+        public string LocationName { get; set; }
+        public string LocationDescription { get; set; }
+        public int Area { get; set; }
+        public int Capacity { get; set; }
+        public int Cost { get; set; }
+        public byte[] LocationImage { get; set; }
+        public int AdminId { get; set; }
+    }
+}
