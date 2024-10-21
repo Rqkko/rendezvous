35056d9cd6a8a2f59ab5832b53e348b6527bc26b
Rqkko <rakko26@gmail.com>
2024-10-21 10:28:50 +0700

    Update EventController.cs
    - Add GetLocation(locationId) method

--- RendezvousApp.Server/Controllers/EventController.cs
+++ RendezvousApp.Server/Controllers/EventController.cs
@@ -75,6 +75,53 @@ public class EventController : ControllerBase
         return Ok();
     }
 
+    [HttpGet("GetLocation/{locationId}")]
+    public ActionResult GetLocation([FromRoute] int locationId)
+    {
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            string query = @"
+                SELECT L.*, A.* FROM Locations AS L
+                JOIN Addresses AS A ON L.locationId=A.locationId
+                WHERE L.locationId=@locationId
+            ";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@locationId", locationId);
+
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    byte[] locationImageBytes = (byte[])reader["locationImage"];
+                    string locationImageBase64 = Convert.ToBase64String(locationImageBytes);
+
+                    Location location = new Location
+                    {
+                        LocationId = (int)reader["locationId"],
+                        LocationName = (string)reader["locationName"],
+                        LocationDescription = (string)reader["locationDescription"],
+                        Area = (int)reader["area"],
+                        Capacity = (int)reader["capacity"],
+                        Cost = (int)reader["cost"],
+                        LocationImage = locationImageBase64,
+                        Province = (string)reader["province"],
+                        PostalCode = (string)reader["postalCode"],
+                        Additional = (string)reader["additional"],
+                        AdminId = (int)reader["adminId"],
+                    };
+
+                    return Ok(location);
+                }
+                else
+                {
+                    return NotFound(new { message = "Location not found" });
+                }
+            }
+        }
+    }
+
     [HttpGet("GetUserReservations")]
     public ActionResult GetUserReservations()
     {
