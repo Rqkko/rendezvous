790222db3b4fa526cbb371df7447c599b7e48c7d
Rqkko <rakko26@gmail.com>
2024-10-21 10:56:04 +0700

    Update Location.tsx
    - Fetch Location from backend to Location.tsx

--- rendezvousapp.client/src/views/Location.tsx
+++ rendezvousapp.client/src/views/Location.tsx
@@ -1,15 +1,37 @@
-import React, { useEffect } from 'react'
+import { Box, Container } from '@mui/material';
+import React, { useEffect, useState } from 'react'
 
 interface LocationProps {
     locationId: string | undefined;
 }
 
+interface Location {
+    locationId: number,
+    locationName: string,
+    locationDescription: string,
+    area: number,
+    capacity: number,
+    cost: number,
+    locationImage: string,
+    province: string,
+    postalCode: string,
+    additional: string,
+    adminId: string
+}
 
 function Location({ locationId }: LocationProps): JSX.Element {
+    const [location, setLocation] = useState<Location | undefined>(undefined);
+
     function fetchLocationDetail(locationId: string): void {
-        console.log("fetchLocationDetail is called")
+        fetch(`/api/event/getlocation/${locationId}`)
+            .then(response => response.json())
+            .then(data => {
+                setLocation(data)
+            })
+            .catch(error => {
+                console.error('Error fetching location details:', error);
+            });
     }
-    
     
     useEffect(() => {
         if (locationId !== undefined) {
@@ -18,7 +40,7 @@ function Location({ locationId }: LocationProps): JSX.Element {
     })
     
     
-    if (locationId === undefined) {
+    if (location === undefined) {
         return (
             <>
                 <div>Location Not Found</div>
@@ -27,11 +49,27 @@ function Location({ locationId }: LocationProps): JSX.Element {
     }
 
     return (
-        <>
+        <Container>
             <div>Location</div>
 
-            <div>{locationId}</div>
-        </>
+            <div>{location.locationName}</div>
+            <div>{location.locationDescription}</div>
+            <div>{location.area}</div>
+            <div>{location.capacity}</div>
+            <div>{location.cost}</div>
+            <div>{location.province}</div>
+            <div>{location.postalCode}</div>
+            <div>{location.additional}</div>
+            <div>{location.adminId}</div>
+
+            <Box sx={{ 
+            height: '300px',
+            width: '300px',
+            backgroundImage: location.locationImage ? `url(data:image/jpeg;base64,${location.locationImage})` : 'none', 
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+            }} />
+        </Container>
     )
 }
 
