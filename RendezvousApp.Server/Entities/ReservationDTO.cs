7c4c03f43e0e5a4e40c1ea4c825d0277d5b90bf9
Rqkko <rakko26@gmail.com>
2024-10-28 13:45:20 +0700

    Update ReservationDTO.cs
    - Add LocationId into ReservationDTO

--- RendezvousApp.Server/Entities/ReservationDTO.cs
+++ RendezvousApp.Server/Entities/ReservationDTO.cs
@@ -2,6 +2,7 @@ namespace RendezvousApp.Server.Entities;
 
 public class ReservationDTO
 {
+    public int LocationId { get; set; }
     public Event Event { get; set; }
     public Reservation Reservation { get; set; }
     public Payment Payment { get; set; }
