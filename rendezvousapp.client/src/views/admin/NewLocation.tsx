6fbaf498d2fbe76da917782abc15a7632091945c
Rqkko <rakko26@gmail.com>
2024-11-06 14:42:27 +0700

    Update NewLocation.tsx
    Copy from Location.tsx

--- rendezvousapp.client/src/views/admin/NewLocation.tsx
+++ rendezvousapp.client/src/views/admin/NewLocation.tsx
@@ -1,9 +1,44 @@
+import { Box, Container, Typography } from '@mui/material'
+import Grid from '@mui/material/Grid2'
 import React from 'react'
 
 function NewLocation() {
     return (
-        <div>NewLocation</div>
-    )
+        <Container 
+            maxWidth="xl"
+            sx={{ px:1}}       
+        >
+            <Box sx={{ my: 4 }}>
+                <Grid container spacing={4}>
+                    {/* Location Image */}
+                    <Grid size={12}>
+                        <Box
+                            sx={{
+                                height: '300px',
+                                width: '100%',
+                                // backgroundImage: location.locationImage ? `url(data:image/jpeg;base64,${location.locationImage})` : 'none',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                borderRadius: 2,
+                            }}
+                        />
+                    </Grid>
+
+                    {/* Location Details */}
+                    <Grid size={{ xs: 12, md: 7 }}>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start', gap: 2 }}>
+                            <Typography variant="h2" align="left">location.locationName</Typography>
+                            <Typography variant="body1" align="left"><strong>Location:</strong> {"location.additional}, {location.province}, {location.postalCode"}</Typography>
+                            <Typography variant="body1" align="left"><strong>Description:</strong>location.locationDescription</Typography>
+                            <Typography variant="body1" align="left"><strong>Area:</strong>location.area square meters</Typography>
+                            <Typography variant="body1" align="left"><strong>Capacity:</strong>location.capacity people</Typography>
+                            <Typography variant="body1" align="left"><strong>Cost:</strong>location.cost Baht/Event</Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
+    );
 }
 
 export default NewLocation
