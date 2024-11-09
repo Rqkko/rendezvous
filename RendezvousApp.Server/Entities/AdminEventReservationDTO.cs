6c676a64bdb446213207fd2bd3d27439b5c8e01c
Rqkko <rakko26@gmail.com>
2024-11-09 21:34:38 +0700

    feat: create AdminEventReservationDTO.cs

---
+++ RendezvousApp.Server/Entities/AdminEventReservationDTO.cs
@@ -0,0 +1,17 @@
+namespace RendezvousApp.Server.Entities
+{
+    // For returning to frontend
+    public class AdminEventReservationDTO
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string LocationName { get; set; }
+        public string LocationImage { get; set; }
+        public string Province { get; set; }
+        public string EventName { get; set; }
+        public string Theme { get; set; }
+        public int GuestCount { get; set; }
+        public DateOnly Date { get; set; }
+        public DateTime ReservationDateTime { get; set; }
+    }
+}
